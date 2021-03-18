a03874f7f6442bc9d002127daf4ab403dcfbb85a
PasinduManeka <manekaherat815@gmail.com>
2021-03-19 01:06:07 +0530

    create service and servlet classes

---
+++ src/com/service/updateItemService.java
@@ -0,0 +1,49 @@
+package com.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.model.Items;
+import com.util.DatabaseConnection;
+
+public class updateItemService {
+	
+	private static Connection con;
+	private static PreparedStatement ps;
+	private static Statement stmt;
+	
+	
+	public void updateItems(Items i) {
+		try {
+			
+			con=DatabaseConnection.getConnection();
+			stmt=con.createStatement();
+			ps=con.prepareStatement("update items set itemCode =?, itemName=?, itemPrice=?, itemDesc=? where itemID=?");
+			
+			ps.setString(1, i.getItemCode());
+			ps.setString(2, i.getItemName());
+			ps.setFloat(3, i.getPrice());
+			ps.setString(4, i.getDescription());
+			ps.setInt(5, i.getItemID());
+			ps.executeUpdate();
+			con.setAutoCommit(false);
+			con.commit();
+			
+			
+		}catch(Exception e) {
+			System.out.print("Database does not connect. "+e);
+			e.printStackTrace();
+		}finally {
+			if(con!=null) {
+				try {
+					con.close();
+				}catch(SQLException e){
+					
+				}
+			}
+		}
+	}
+
+}
---
+++ src/com/servlet/updateItemServlet.java
@@ -0,0 +1,58 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.model.Items;
+import com.service.updateItemService;
+
+/**
+ * Servlet implementation class updateItemServlet
+ */
+@WebServlet("/updateItemServlet")
+public class updateItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		int id = Integer.parseInt(request.getParameter("itemID"));
+		String itemCode = request.getParameter("iCode");
+		String itemName = request.getParameter("iName");
+		float itemPrice = Float.parseFloat(request.getParameter("iPrice"));
+		String itemDescription =request.getParameter("iDescription");
+		
+		Items it = new Items();
+		
+		it.setItemID(id);
+		it.setItemCode(itemCode);
+		it.setItemName(itemName);
+		it.setPrice(itemPrice);
+		it.setDescription(itemDescription);
+		
+		updateItemService ui = new updateItemService();
+		ui.updateItems(it);
+		
+		RequestDispatcher dis = request.getRequestDispatcher("itemsView.jsp");
+		dis.forward(request,response);
+	}
+
+}
