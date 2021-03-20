1ef978ee4d0bbfe4a89497f9d059a84949b1f512
PasinduManeka <manekaherat815@gmail.com>
2021-03-21 02:50:59 +0530

    did changes

--- src/com/model/Items.java
+++ src/com/model/Items.java
@@ -1,42 +1,45 @@
 package com.model;
 
+
 public class Items {
-	private int itemID;
-	private String itemCode;
-	private String itemName;
-	private float price;
-	private String description;
-	
-	public int getItemID() {
-		return itemID;
-	}
-	public void setItemID(int itemID) {
-		this.itemID = itemID;
-	}
-	public String getItemCode() {
-		return itemCode;
-	}
-	public void setItemCode(String itemCode) {
-		this.itemCode = itemCode;
-	}
-	public String getItemName() {
-		return itemName;
-	}
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-	public float getPrice() {
-		return price;
-	}
-	public void setPrice(float price) {
-		this.price = price;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	
+		private int itemID;
+		private String itemCode;
+		private String itemName;
+		private float price;
+		private String description;
+		
+		public int getItemID() {
+			return itemID;
+		}
+		public void setItemID(int itemID) {
+			this.itemID = itemID;
+		}
+		public String getItemCode() {
+			return itemCode;
+		}
+		public void setItemCode(String itemCode) {
+			this.itemCode = itemCode;
+		}
+		public String getItemName() {
+			return itemName;
+		}
+		public void setItemName(String itemName) {
+			this.itemName = itemName;
+		}
+		public float getPrice() {
+			return price;
+		}
+		public void setPrice(float price) {
+			this.price = price;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		
+		
 }
+
+
